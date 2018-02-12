733be5d3f5591de365437e2a5abb1944861320f8
Ken Siu <ken.siu@accedo.tv>
2018-02-12 10:51:04 +0800

    initial commit

---
+++ CodableInAction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodableInAction
+//
+//  Created by Ken Siu on 12/2/2018.
+//  Copyright © 2018 Ken Siu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
